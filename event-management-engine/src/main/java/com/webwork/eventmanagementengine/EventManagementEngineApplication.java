10885200f13eea65197e81553f7825f3493c55a8
sunilbombe456 <sunilbombe456@gmail.com>
2020-10-21 19:20:20 +0530

    First Commit

---
+++ event-management-engine/src/main/java/com/webwork/eventmanagementengine/EventManagementEngineApplication.java
@@ -0,0 +1,13 @@
+package com.webwork.eventmanagementengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementEngineApplication.class, args);
+	}
+
+}
